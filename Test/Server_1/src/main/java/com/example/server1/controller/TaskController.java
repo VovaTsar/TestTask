970a415aecdc715549cc120b40b1d234a80dc2f3
VovaTsar <dropttecttt@mail.ru>
2019-07-04 01:06:18 +0300

    fix response

--- Test/Server_1/src/main/java/com/example/server1/controller/TaskController.java
+++ Test/Server_1/src/main/java/com/example/server1/controller/TaskController.java
@@ -9,17 +9,22 @@ import java.util.*;
 
 @RestController
 public class TaskController {
-    private List<Product> result;
+    private List<Product> response;
     private Queue<Task> queue = new LinkedList<>();
 
     @PostMapping("/api/execute")
-    public void execute(@RequestBody Task task) {
+    public String execute(@RequestBody Task task) {
         queue.add(task);
         int count = 0;
         for (Task element : queue) {
             count++;
             System.out.println(count + element.toString());
         }
+        while (response ==null){
+        }
+        List<Product> temp = response;
+        response = null;
+        return temp.toString();
     }
 
     @GetMapping("/api/getTask")
@@ -29,21 +34,10 @@ public class TaskController {
 
     @PostMapping("/api/putResult")
     public void putResult(@RequestBody List<Product> products) {
-        result = new ArrayList<>(products);
+        response= new ArrayList<>(products);
         for (Product i : products) {
             System.out.println(i);
         }
     }
-
-    @GetMapping("/api/execute")
-    public String execute() {
-        return result.toString();
-    }
-
-    @GetMapping("/api/putResult")
-    public String putResult1() {
-        //model.addAttribute("products",products);
-        return result.toString();
-        // return "redirect:/api/putResult";
-    }
+    
 }
--- Test/Server_2/src/main/java/com/example/server2/Server2Application.java
+++ Test/Server_2/src/main/java/com/example/server2/Server2Application.java
@@ -12,4 +12,3 @@ public class Server2Application {
     }
 
 }
-
